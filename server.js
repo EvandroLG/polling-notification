6d948661450136b80c10b9f55240b1c3f06d68fe
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2017-09-24 23:52:53 -0300

    named main directory to public

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'app/assets/')));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/app/index.html')));
+app.use(express.static(path.join(__dirname, 'public/assets/')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
 app.get('/api/', (req, res) => {
   const date = new Date();
